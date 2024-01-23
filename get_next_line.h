8432b02769acb3aba969f1e4bd5c6b789a1c87e0
ugisar <nikola.saranchimeg@seznam.cz>
2024-01-23 12:05:14 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: usanch <usaranch@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 11:49:27 by usanch            #+#    #+#             */
+/*   Updated: 2024/01/23 11:59:00 by usanch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*get_string(char *str);
+char	*read_the_line(char *str);
+char	*free_and_null(char *buff1, char *buff2);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strdup(const char *src);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: usanch <usaranch@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 11:49:33 by usanch            #+#    #+#             */
+/*   Updated: 2024/01/23 11:59:10 by usanch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] == (char)c)
+			return ((char *)&str[i]);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)&str[i]);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = ft_strlen(s);
+	str = (char *)malloc(sizeof(*str) * (j + 1));
+	if (!str)
+		return (NULL);
+	while (i < j)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char *buffer, char *content)
+{
+	size_t	i;
+	size_t	j;
+	char	*result;
+
+	if (!buffer)
+		return (ft_strdup(content));
+	if (!content)
+		return (ft_strdup(buffer));
+	i = 0;
+	result = (char *)malloc(sizeof(char) * (ft_strlen(buffer)
+				+ ft_strlen(content)) + 1);
+	if (!result)
+		return (NULL);
+	while (buffer && buffer[i])
+	{
+		result[i] = buffer[i];
+		i++;
+	}
+	j = 0;
+	while (content && content[j])
+		result[i++] = content[j++];
+	result[i] = '\0';
+	free(buffer);
+	return (result);
+}
